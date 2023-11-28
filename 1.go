d29cd9b9f4805f46349fcc916c497db6d9a12674
RomanMalashenkov <malashenckov.roma@yandex.ru>
2023-11-28 12:01:47 +0300

    asd

--- 1.go
+++ 1.go
@@ -6,6 +6,32 @@
 
 package main
 
-func main() {
+import "fmt"
 
+type Human struct {
+	FirstName string
+	LastName  string
+	secret    string
+}
+
+func (h *Human) MyName() string {
+	return fmt.Sprintf("%s %s", h.FirstName, h.LastName)
+}
+
+type Action struct {
+	ActionName string
+	// композиция структур
+	Human
+}
+
+func main() {
+	action := Action{
+		ActionName: "humor",
+		Human:      Human{FirstName: "Mr", LastName: "Bean"},
+	}
+
+	// в рамках этого пакета Private поле доступно, в других - недоступно.
+	action.secret = "**************************"
+
+	fmt.Printf("Task1. %s doing %s \n", action.MyName(), action.ActionName)
 }
